570cbee36379bb0be36856e850e322340842d530
liarles <62631264+liarles@users.noreply.github.com>
2024-12-06 22:56:47 +0700

    Update lab8_2.cpp

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -3,7 +3,27 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double Grade){ //[Missing Code 1] Write definition of the function findGrade() here.
+
+    char x;
+    if (Grade > 90){
+    x='A';
+    }
+    else if (Grade <=90 && Grade > 75){
+    x='B';
+    }
+    else if (Grade <=75 && Grade > 60){
+    x='C';
+    }
+    else if (Grade <=60 && Grade > 45){
+    x='D';
+    }
+    else if (Grade <=45){
+    x='F';
+    }
+    return x;
+}
+
 
 int main(){
 	//Input the number of students
@@ -17,9 +37,9 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin, name[i]); //[Missing Code 2] Get name of the i-th students that may include whitespace.
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];  //[Missing Code 3] Get score of the i-th students.
 		i++;
 	}
 	
